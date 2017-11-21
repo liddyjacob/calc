26eaf27015232d3e77fcf0b9013dc89ee670e0a2
Jacob Liddy <liddyjacob@gmail.com>
2017-11-20 19:52:28 -0500

    Started virtual implementation of calc

--- virtual/calc.hpp
+++ virtual/calc.hpp
@@ -1,88 +1,15 @@
+//Virtual implementation of calculator function - 
 
 struct Expr;
 struct Int;
+struct Mult;
 struct Add;
-struct Sub;
 
 
-struct Visitor
-{
-	virtual void visit(Int* e) = 0;
-	virtual void visit(Add* e) = 0;
-	virtual void visit(Sub* e) = 0;
+struct Expr { 
+	virtual ~Expr() = 0;
+	virtual Expr* clone() = 0;
+	virtual void print() = 0;
+	virtual int eval() = 0;
 }
 
-struct EvalVisitor : Visitor
-{
-	int ret;
-
-	void visit(Int* e) override {
-		ret = e->val;
-	}
-
-	void visit(Add* e) override {
-		EvalVisitor v1;
-		e->e1->accept(v1);
-
-		EvalVisitor v2;
-		e->e2->accept(v2);
-
-		ret = v1.ret + v2.ret;
-	
-	}
-
-	void visit(Sub* e) override {
-		EvalVisitor v1;
-		e->e1->accept(v1);
-
-		EvalVisitor v2;
-		e->e2->accept(v2);
-
-		ret = v1.ret - v2.ret;	
-	}
-
-
-}
-
-
-struct Expr{
-
-	virtual ~Expr() = default;
-};
-
-struct Int : Expr
-{
-	Int(int n)
-		:val(n)
-	{ }
-
-	Int (const Int& n):
-		:val(n.n)
-	{ }
-
-	void accept(Visitor& v) {v.visit(this);}
-
-	int val;
-};
-
-
-struct Binary: Expr
-{
-
-
-	virtual void accept(Visitor& v) =0;
-};
-
-
-
-struct Add : Binary{
-	using Binary::Binary;
-
-	void 
-
-};
-
-struct Sub : Binary{
-	using Binary::Binary();
-
-};
